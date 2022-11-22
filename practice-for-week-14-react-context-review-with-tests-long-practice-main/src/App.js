5939aa8689b254d004e8130a2128d9d68da3f250
Arnob <arnobdam@gmail.com>
2022-11-22 14:27:41 -0500

    finished phase 1

--- practice-for-week-14-react-context-review-with-tests-long-practice-main/src/App.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice-main/src/App.js
@@ -1,8 +1,10 @@
 import coffeeBeans from "./mockData/coffeeBeans.json";
+import SelectedCoffeeBean from "./components/SelectedCoffeeBean";
 
 function App() {
   return (
     <>
+    <SelectedCoffeeBean/>
       <h1>Welcome to Coffee App</h1>
     </>
   );
